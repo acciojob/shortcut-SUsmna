fea3897f2977b221a163e6bd24ff81f2817eddcb
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-15 15:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1 ==="" || s2===""){
+		return '';
+	}
+	const letters = s1.charAt(0).toLowerCase() + s2.charAt(0).toLowerCase();
+	return letters
 }
 
 // Do not change the code below.
